b9d5fcf12252bc09f078bcb1f1776b0f427c2e92
Pulimet <korolev2009@gmail.com>
2022-06-10 12:50:30 +0300

    Show YouTube player in RecyclerView

--- app/src/main/java/com/att/tapyou/ui/home/VideoHolder.kt
+++ app/src/main/java/com/att/tapyou/ui/home/VideoHolder.kt
@@ -1,5 +1,6 @@
 package com.att.tapyou.ui.home
 
+import android.annotation.SuppressLint
 import android.view.View
 import androidx.recyclerview.widget.RecyclerView
 import com.att.tapyou.databinding.ItemVideoBinding
@@ -8,8 +9,14 @@ class VideoHolder(v: View) : RecyclerView.ViewHolder(v) {
 
     private val binding = ItemVideoBinding.bind(v)
 
+    @SuppressLint("SetJavaScriptEnabled")
     fun onBindViewHolder(id: String?) {
-        binding.tvId.text = id
+        val data =
+            "<iframe width='560' height='315' src='https://www.youtube.com/embed/$id' frameborder='0' allow='accelerometer; encrypted-media; gyroscope; picture-in-picture' allowfullscreen></iframe>"
+        binding.webView.apply {
+            settings.javaScriptEnabled = true
+            loadData(data, "text/html; charset=utf-8", "UTF-8")
+        }
     }
 
 }
